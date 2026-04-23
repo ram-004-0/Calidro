1291b60cb86046f683bc950b170a00be063afcc2
Ram <garingan.280431@globalcity.sti.edu.ph>
2026-04-23 23:50:40 +0800

    adsdaaaa

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ app.use(
 // In server.js, add this middleware near your CORS settings
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
   next();
 });
 // --- 2. BODY PARSERS ---
