a3cc810a26e61c13c9facde120016baeb89373ab
Ram <garingan.280431@globalcity.sti.edu.ph>
2026-04-24 02:56:08 +0800

    fha

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.PORT) || 3306, // Ensure it's a number
+  port: 24097,
   ssl: { rejectUnauthorized: false },
 });
 // Add this temporarily above the pool creation
