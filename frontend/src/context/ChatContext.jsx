ceab89790ce6b38c4f2162b1dca87d8e35ad35c2
Ram <garingan.280431@globalcity.sti.edu.ph>
2026-04-24 03:10:48 +0800

    ha

--- frontend/src/context/ChatContext.jsx
+++ frontend/src/context/ChatContext.jsx
@@ -7,7 +7,7 @@ import React, {
 } from "react";
 import { io } from "socket.io-client";
 //tangina talaga
-const API_URL = "calidro-production.up.railway.app";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 const socket = io(API_URL);
 
 const ChatContext = createContext();
