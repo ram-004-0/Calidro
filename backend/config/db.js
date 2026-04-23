1ddb01e34191c48869edc3f67cabeb28f8b8b21b
Ram <garingan.280431@globalcity.sti.edu.ph>
2026-04-24 02:10:06 +0800

    pls lang

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,13 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false, // This is mandatory for Aiven/cloud MySQL
+  },
 });
 
 module.exports = pool.promise();
